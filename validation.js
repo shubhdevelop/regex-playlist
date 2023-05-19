5b98685948f3ead520a459104cc25e48583d4a47
shubham <49603826+shubhdevelop@users.noreply.github.com>
2023-05-19 23:25:21 +0530

    feat[form validation]
    - wrote regex rule for validating form inputs
    - and depending on the return of the validation function add class
    "valid" or "invalid" which as effect changes the styling of the form
    accordingly

--- validation.js
+++ validation.js
@@ -1,1 +1,32 @@
 // validation script here
+// let reg = /[a-z]/gi;
+// let reg2 = new RegExp(/[a-z]/,'i'); 
+
+
+const inputs = document.querySelectorAll('input');
+
+const patterns={
+    telephone:/^(\+[\d]{1,2}\s)?\d{10}$/,
+    username:/^[a-z\d]{5,12}$/i,
+    password:/^[\w]{8,20}$/,
+    slug:/^[a-z\d-]{8,20}$/,
+    email:/^([\w-]+)@([\w])+\.([\w]{2,8})(\.[a-z]{2,8})?$/
+
+
+}
+
+//validation fuction
+function validate(field, regex){
+   let result = regex.test(field.value, regex);
+   result ? field.className = "valid":
+            field.className = "invalid";
+}
+
+
+inputs.forEach((input)=>{
+    input.addEventListener('keyup', e => {
+        // console.log(e.target.attributes.name.value);
+        validate(e.target, patterns[e.target.attributes.name.value]);
+    })
+})
+
